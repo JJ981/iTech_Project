913465278eb7d6b98d5661e728b885d47d696feb
GAMER <GAMER@DESKTOP-SCA0ECD>
2024-03-27 16:02:44 -0300

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace iTech_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
